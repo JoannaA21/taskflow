51fd2d70f295d971d267a9c985649f65e44280ee
Joanna Apellido <joanne14.ja@gmail.com>
2026-03-04 14:41:11 -0700

    Refactor project with test using jest and supertest. Separated app config and server startup

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const routes = require("./routes/routes");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api", routes);
+
+module.exports = app;
---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on port", process.env.PORT);
+    });
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => console.error("MongoDB connection error:", error));
